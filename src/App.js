f8380866885cba6ca953ee71be0ab1d9bf2ae659
Chase Derr <Chisnits@users.noreply.github.com>
2017-12-06 20:43:42 -0700

    work

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <div className="header-container">
           <div className="nike-swoosh-logo">
-          
+          Chase
         </div>
           cool
         </div>
